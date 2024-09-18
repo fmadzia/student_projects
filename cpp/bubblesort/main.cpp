4f72e3a4c189f40a887d5b9f3ce39c26968fd09e
fmadzia <fmadzia_211p@e.zseeim.edu.pl>
2024-09-18 07:29:14 +0000

    studentmachine send

--- cpp/bubblesort/main.cpp
+++ cpp/bubblesort/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-	cout<<"witaj swiecie"<<endl;
-	return 0;
+void printVector(vector<int> x) {
+	for(size_t i = 0; i < x.size(); i++) {
+		cout<<x[i]<<" ";
+	}
+
+	cout<<endl;
+}
+
+void bubbleSort(vector<int>& x) {
+	bool swap = true;
+
+	while(swap) {
+		swap = false;
+
+		for(size_t i = 0; i < x.size() - 1; i++) {
+			if(x[i] > x[i + 1]) {
+				x[i] += x[i + 1];
+				x[i + 1] = x[i] - x[i + 1];
+				x[i] -= x[i + 1];
+
+				swap = true;
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	vector<int> x { 3, 1, 5, 4 };
+
+	printVector(x);
+
+	bubbleSort(x);
+
+	printVector(x);
 }
