ccedd59b2683448edc4a6c60823386af56fcec4e
fmadzia <fmadzia_211p@e.zseeim.edu.pl>
2024-10-02 07:48:55 +0200

    Add files via upload

---
+++ cpp/benchmark_if_switch/main_test.cpp
@@ -0,0 +1,513 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include "gtest/gtest.h"
+#include <chrono>
+
+using namespace std;
+
+int num = 0;
+
+void test_if(int num)
+{
+    if (num == 0) {
+        cout << "num == 0" << endl;
+    }
+    else if (num == 1) {
+        cout << "num == 1" << endl;
+    }
+    else if (num == 2) {
+        cout << "num == 2" << endl;
+    }
+    else if (num == 3) {
+        cout << "num == 3" << endl;
+    }
+    else if (num == 4) {
+        cout << "num == 4" << endl;
+    }
+    else if (num == 5) {
+        cout << "num == 5" << endl;
+    }
+    else if (num == 6) {
+        cout << "num == 6" << endl;
+    }
+    else if (num == 7) {
+        cout << "num == 7" << endl;
+    }
+    else if (num == 8) {
+        cout << "num == 8" << endl;
+    }
+    else if (num == 9) {
+        cout << "num == 9" << endl;
+    }
+    else if (num == 10) {
+        cout << "num == 10" << endl;
+    }
+    else if (num == 11) {
+        cout << "num == 11" << endl;
+    }
+    else if (num == 12) {
+        cout << "num == 12" << endl;
+    }
+    else if (num == 13) {
+        cout << "num == 13" << endl;
+    }
+    else if (num == 14) {
+        cout << "num == 14" << endl;
+    }
+    else if (num == 15) {
+        cout << "num == 15" << endl;
+    }
+    else if (num == 16) {
+        cout << "num == 16" << endl;
+    }
+    else if (num == 17) {
+        cout << "num == 17" << endl;
+    }
+    else if (num == 18) {
+        cout << "num == 18" << endl;
+    }
+    else if (num == 19) {
+        cout << "num == 19" << endl;
+    }
+    else if (num == 20) {
+        cout << "num == 20" << endl;
+    }
+    else if (num == 21) {
+        cout << "num == 21" << endl;
+    }
+    else if (num == 22) {
+        cout << "num == 22" << endl;
+    }
+    else if (num == 23) {
+        cout << "num == 23" << endl;
+    }
+    else if (num == 24) {
+        cout << "num == 24" << endl;
+    }
+    else if (num == 25) {
+        cout << "num == 25" << endl;
+    }
+    else if (num == 26) {
+        cout << "num == 26" << endl;
+    }
+    else if (num == 27) {
+        cout << "num == 27" << endl;
+    }
+    else if (num == 28) {
+        cout << "num == 28" << endl;
+    }
+    else if (num == 29) {
+        cout << "num == 29" << endl;
+    }
+    else if (num == 30) {
+        cout << "num == 30" << endl;
+    }
+    else if (num == 31) {
+        cout << "num == 31" << endl;
+    }
+    else if (num == 32) {
+        cout << "num == 32" << endl;
+    }
+    else if (num == 33) {
+        cout << "num == 33" << endl;
+    }
+    else if (num == 34) {
+        cout << "num == 34" << endl;
+    }
+    else if (num == 35) {
+        cout << "num == 35" << endl;
+    }
+    else if (num == 36) {
+        cout << "num == 36" << endl;
+    }
+    else if (num == 37) {
+        cout << "num == 37" << endl;
+    }
+    else if (num == 38) {
+        cout << "num == 38" << endl;
+    }
+    else if (num == 39) {
+        cout << "num == 39" << endl;
+    }
+    else if (num == 40) {
+        cout << "num == 40" << endl;
+    }
+    else if (num == 41) {
+        cout << "num == 41" << endl;
+    }
+    else if (num == 42) {
+        cout << "num == 42" << endl;
+    }
+    else if (num == 43) {
+        cout << "num == 43" << endl;
+    }
+    else if (num == 44) {
+        cout << "num == 44" << endl;
+    }
+    else if (num == 45) {
+        cout << "num == 45" << endl;
+    }
+    else if (num == 46) {
+        cout << "num == 46" << endl;
+    }
+    else if (num == 47) {
+        cout << "num == 47" << endl;
+    }
+    else if (num == 48) {
+        cout << "num == 48" << endl;
+    }
+    else if (num == 49) {
+        cout << "num == 49" << endl;
+    }
+    else if (num == 50) {
+        cout << "num == 50" << endl;
+    }
+    else if (num == 51) {
+        cout << "num == 51" << endl;
+    }
+    else if (num == 52) {
+        cout << "num == 52" << endl;
+    }
+    else if (num == 53) {
+        cout << "num == 53" << endl;
+    }
+    else if (num == 54) {
+        cout << "num == 54" << endl;
+    }
+    else if (num == 55) {
+        cout << "num == 55" << endl;
+    }
+    else if (num == 56) {
+        cout << "num == 56" << endl;
+    }
+    else if (num == 57) {
+        cout << "num == 57" << endl;
+    }
+    else if (num == 58) {
+        cout << "num == 58" << endl;
+    }
+    else if (num == 59) {
+        cout << "num == 59" << endl;
+    }
+    else if (num == 60) {
+        cout << "num == 60" << endl;
+    }
+    else if (num == 61) {
+        cout << "num == 61" << endl;
+    }
+    else if (num == 62) {
+        cout << "num == 62" << endl;
+    }
+    else if (num == 63) {
+        cout << "num == 63" << endl;
+    }
+    else if (num == 64) {
+        cout << "num == 64" << endl;
+    }
+    else if (num == 65) {
+        cout << "num == 65" << endl;
+    }
+    else if (num == 66) {
+        cout << "num == 66" << endl;
+    }
+    else if (num == 67) {
+        cout << "num == 67" << endl;
+    }
+    else if (num == 68) {
+        cout << "num == 68" << endl;
+    }
+    else if (num == 69) {
+        cout << "num == 69" << endl;
+    }
+    else if (num == 70) {
+        cout << "num == 70" << endl;
+    }
+    else if (num == 71) {
+        cout << "num == 71" << endl;
+    }
+    else if (num == 72) {
+        cout << "num == 72" << endl;
+    }
+    else if (num == 73) {
+        cout << "num == 73" << endl;
+    }
+    else if (num == 74) {
+        cout << "num == 74" << endl;
+    }
+    else if (num == 75) {
+        cout << "num == 75" << endl;
+    }
+}
+
+void test_case(int num)
+{
+    case 0:
+        cout << "num == 0" << endl;
+        break;
+    case 1:
+        cout << "num == 1" << endl;
+        break;
+    case 2:
+        cout << "num == 2" << endl;
+        break;
+    case 3:
+        cout << "num == 3" << endl;
+        break;
+    case 4:
+        cout << "num == 4" << endl;
+        break;
+    case 5:
+        cout << "num == 5" << endl;
+        break;
+    case 6:
+        cout << "num == 6" << endl;
+        break;
+    case 7:
+        cout << "num == 7" << endl;
+        break;
+    case 8:
+        cout << "num == 8" << endl;
+        break;
+    case 9:
+        cout << "num == 9" << endl;
+        break;
+    case 10:
+        cout << "num == 10" << endl;
+        break;
+    case 11:
+        cout << "num == 11" << endl;
+        break;
+    case 12:
+        cout << "num == 12" << endl;
+        break;
+    case 13:
+        cout << "num == 13" << endl;
+        break;
+    case 14:
+        cout << "num == 14" << endl;
+        break;
+    case 15:
+        cout << "num == 15" << endl;
+        break;
+    case 16:
+        cout << "num == 16" << endl;
+        break;
+    case 17:
+        cout << "num == 17" << endl;
+        break;
+    case 18:
+        cout << "num == 18" << endl;
+        break;
+    case 19:
+        cout << "num == 19" << endl;
+        break;
+    case 20:
+        cout << "num == 20" << endl;
+        break;
+    case 21:
+        cout << "num == 21" << endl;
+        break;
+    case 22:
+        cout << "num == 22" << endl;
+        break;
+    case 23:
+        cout << "num == 23" << endl;
+        break;
+    case 24:
+        cout << "num == 24" << endl;
+        break;
+    case 25:
+        cout << "num == 25" << endl;
+        break;
+    case 26:
+        cout << "num == 26" << endl;
+        break;
+    case 27:
+        cout << "num == 27" << endl;
+        break;
+    case 28:
+        cout << "num == 28" << endl;
+        break;
+    case 29:
+        cout << "num == 29" << endl;
+        break;
+    case 30:
+        cout << "num == 30" << endl;
+        break;
+    case 31:
+        cout << "num == 31" << endl;
+        break;
+    case 32:
+        cout << "num == 32" << endl;
+        break;
+    case 33:
+        cout << "num == 33" << endl;
+        break;
+    case 34:
+        cout << "num == 34" << endl;
+        break;
+    case 35:
+        cout << "num == 35" << endl;
+        break;
+    case 36:
+        cout << "num == 36" << endl;
+        break;
+    case 37:
+        cout << "num == 37" << endl;
+        break;
+    case 38:
+        cout << "num == 38" << endl;
+        break;
+    case 39:
+        cout << "num == 39" << endl;
+        break;
+    case 40:
+        cout << "num == 40" << endl;
+        break;
+    case 41:
+        cout << "num == 41" << endl;
+        break;
+    case 42:
+        cout << "num == 42" << endl;
+        break;
+    case 43:
+        cout << "num == 43" << endl;
+        break;
+    case 44:
+        cout << "num == 44" << endl;
+        break;
+    case 45:
+        cout << "num == 45" << endl;
+        break;
+    case 46:
+        cout << "num == 46" << endl;
+        break;
+    case 47:
+        cout << "num == 47" << endl;
+        break;
+    case 48:
+        cout << "num == 48" << endl;
+        break;
+    case 49:
+        cout << "num == 49" << endl;
+        break;
+    case 50:
+        cout << "num == 50" << endl;
+        break;
+    case 51:
+        cout << "num == 51" << endl;
+        break;
+    case 52:
+        cout << "num == 52" << endl;
+        break;
+    case 53:
+        cout << "num == 53" << endl;
+        break;
+    case 54:
+        cout << "num == 54" << endl;
+        break;
+    case 55:
+        cout << "num == 55" << endl;
+        break;
+    case 56:
+        cout << "num == 56" << endl;
+        break;
+    case 57:
+        cout << "num == 57" << endl;
+        break;
+    case 58:
+        cout << "num == 58" << endl;
+        break;
+    case 59:
+        cout << "num == 59" << endl;
+        break;
+    case 60:
+        cout << "num == 60" << endl;
+        break;
+    case 61:
+        cout << "num == 61" << endl;
+        break;
+    case 62:
+        cout << "num == 62" << endl;
+        break;
+    case 63:
+        cout << "num == 63" << endl;
+        break;
+    case 64:
+        cout << "num == 64" << endl;
+        break;
+    case 65:
+        cout << "num == 65" << endl;
+        break;
+    case 66:
+        cout << "num == 66" << endl;
+        break;
+    case 67:
+        cout << "num == 67" << endl;
+        break;
+    case 68:
+        cout << "num == 68" << endl;
+        break;
+    case 69:
+        cout << "num == 69" << endl;
+        break;
+    case 70:
+        cout << "num == 70" << endl;
+        break;
+    case 71:
+        cout << "num == 71" << endl;
+        break;
+    case 72:
+        cout << "num == 72" << endl;
+        break;
+    case 73:
+        cout << "num == 73" << endl;
+        break;
+    case 74:
+        cout << "num == 74" << endl;
+        break;
+    case 75:
+        cout << "num == 75" << endl;
+        break;
+    default:
+        cout << "num is out of range" << endl;
+        break;
+}
+
+TEST(BenchmarkTest, IfBenchmark)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+
+
+	auto end = chrono::high_resolution_clock::now();
+
+	chrono::duration<double> elapsed_seconds = end - start;
+
+	cout << "Czas dla instrukcji case: " << elapsed_seconds.count() << "s" << endl;
+}
+
+TEST(BenchmarkTest, CaseBenchmark)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+
+
+	auto end = chrono::high_resolution_clock::now();
+
+	chrono::duration<double> elapsed_seconds = end - start;
+
+    cout << "Czas dla instrukcji case: " << elapsed_seconds.count() << "s" << endl;
+}
+
+int main(int argc, char **argv)
+{
+	cout << "Podaj liczbe: ";
+	cin >> num;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
