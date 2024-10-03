c34d1f5d24a37fd44a18dbc06b2c1ac2dcc800b1
fmadzia <fmadzia_211p@e.zseeim.edu.pl>
2024-10-03 08:20:51 +0200

    Update functions.cpp

--- cpp/unit_tests_fizzbuzz/functions.cpp
+++ cpp/unit_tests_fizzbuzz/functions.cpp
@@ -1,2 +1,20 @@
 
-#TODO: tu umieść swoją funkcję
+string fizz_buzz(int number)
+{
+  if(number % 5 == 0 && number % 3 == 0)
+  {
+    return "fizz";
+  }
+  else if(number % 3 == 0)
+  {
+    return "fizz";
+  }
+  else if(number % 5 == 0)
+  {
+    return "buzz";
+  }
+  else
+  {
+    return "raah";
+  }
+}
