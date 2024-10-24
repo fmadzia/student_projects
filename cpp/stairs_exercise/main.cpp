32646282caffc71a457b6548f79d2e5a551b6d19
fmadzia <fmadzia_211p@e.zseeim.edu.pl>
2024-10-24 08:27:36 +0200

    Update main.cpp

--- cpp/stairs_exercise/main.cpp
+++ cpp/stairs_exercise/main.cpp
@@ -4,26 +4,26 @@
 using namespace std;
 
 void stairsExercise(int availableDepth, int availableHeight) {
-	const int optimalDepth = 25;
-	const int optimalHeight = 15;
+	const int OPTIMAL_DEPTH = 25;
+	const int OPTIMAL_HEIGHT = 15;
 
-	int stepCount = ceil(availableHeight / optimalHeight);
+	int stepCount = ceil(availableHeight / OPTIMAL_HEIGHT);
 
 	float stepDepth = availableDepth / stepCount;
 	float stepHeight = availableHeight / stepCount;
 
-	if(stepHeight > optimalHeight) {
-		cout << "!!! Obliczona wysokosc schodow (" << stepHeight << ") jest wieksza od optymalnej (" << optimalHeight << ")" << endl;
+	if(stepHeight > OPTIMAL_HEIGHT) {
+		cout << "!!! Obliczona wysokosc schodow (" << stepHeight << ") jest wieksza od optymalnej (" << OPTIMAL_HEIGHT << ")" << endl;
 	}
-	else if(stepHeight < optimalHeight) {
-		cout << "!!! Obliczona wysokosc schodow (" << stepHeight << ") jest mniejsza od optymalnej (" << optimalHeight << ")" << endl;
+	else if(stepHeight < OPTIMAL_HEIGHT) {
+		cout << "!!! Obliczona wysokosc schodow (" << stepHeight << ") jest mniejsza od optymalnej (" << OPTIMAL_HEIGHT << ")" << endl;
 	}
 
-	if(stepDepth > optimalDepth) {
-		cout << "!!! Obliczona glebokosc schodow (" << stepDepth << ") jest wieksza od optymalnej (" << optimalDepth << ")" << endl;
+	if(stepDepth > OPTIMAL_DEPTH) {
+		cout << "!!! Obliczona glebokosc schodow (" << stepDepth << ") jest wieksza od optymalnej (" << OPTIMAL_DEPTH << ")" << endl;
 	}
-	else if(stepDepth < optimalDepth) {
-		cout << "!!! Obliczona glebokosc schodow (" << stepDepth << ") jest mniejsza od optymalnej (" << optimalDepth << ")" << endl;
+	else if(stepDepth < OPTIMAL_DEPTH) {
+		cout << "!!! Obliczona glebokosc schodow (" << stepDepth << ") jest mniejsza od optymalnej (" << OPTIMAL_DEPTH << ")" << endl;
 	}
 
 
